af51c9f83c751ae4e802c0eb926503bf365959d4
anhtaivp92 <anhtaivinhphuc1992@gmail.com>
2024-09-17 21:12:18 +0700

    Update get-rules.sh

--- get-rules.sh
+++ get-rules.sh
@@ -1,4 +1,4 @@
 curl -X GET \
-"https://api.cloudflare.com/client/v4/zones/dc4829573a8be18a7f2de65921259f9e/firewall/rules" \
--H "X-Auth-Email: aulax432@gmail.com" \
--H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3"
+"https://api.cloudflare.com/client/v4/zones/e856eac354b9546912d89f1bcf9c1588/firewall/rules" \
+-H "X-Auth-Email: nguyenanhtai04091992vp@gmail.com" \
+-H "X-Auth-Key: 1a4ca6fd329aa0d875ef065094e66ff21f1ea"
