89ac80d8fab170915c1dde07493bc568685f9fd7
anhtaivp92 <anhtaivinhphuc1992@gmail.com>
2024-09-17 21:27:34 +0700

    Update captcha-off.sh

--- captcha-off.sh
+++ captcha-off.sh
@@ -1,16 +1,16 @@
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/dc4829573a8be18a7f2de65921259f9e/firewall/rules/adb451c64204498d986435b508aa8a0e" \
-     -H "X-Auth-Email: aulax432@gmail.com" \
-     -H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/e856eac354b9546912d89f1bcf9c1588/firewall/rules/b74d02367fc94d5cac551818ec32dc29" \
+     -H "X-Auth-Email: nguyenanhtai04091992vp@gmail.com" \
+     -H "X-Auth-Key: 1a4ca6fd329aa0d875ef065094e66ff21f1ea" \
      -H "Content-Type: application/json" \
      --data '{
      "action": "managed_challenge",
      "priority": 564,
      "paused": true,
-     "description": "Managed Challenge is in Asia",
+     "description": "Captcha Việt Nam",
      "filter": {
-    "id": "bf916bc3ff084f32b44d9ffac1fec3da",
-    "expression": "(http.user_agent contains \"Mozilla/5.0 (iPhone; CPU iPhone OS\" and ip.geoip.country in {\"CN\" \"DE\" \"HK\" \"ID\" \"JP\" \"KP\" \"KR\" \"SG\" \"TW\" \"US\" \"VN\"} and ip.geoip.continent eq \"AS\" and cf.threat_score le 5 and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent contains \"Mozilla/5.0 (Linux; Android\" and ip.geoip.country in {\"CN\" \"DE\" \"HK\" \"ID\" \"JP\" \"KP\" \"KR\" \"TW\" \"US\" \"VN\" \"SG\"} and ip.geoip.continent eq \"AS\" and cf.threat_score le 5 and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"POST\" \"GET\"}) or (http.user_agent contains \"Mozilla/5.0 (Windows NT\" and ip.geoip.country in {\"CN\" \"DE\" \"HK\" \"ID\" \"JP\" \"KP\" \"KR\" \"SG\" \"TW\" \"US\" \"VN\"} and ip.geoip.continent eq \"AS\" and cf.threat_score le 5 and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent contains \"Mozilla/5.0 (Macintosh; Intel Mac OS\" and ip.geoip.country in {\"CN\" \"DE\" \"HK\" \"ID\" \"JP\" \"KP\" \"KR\" \"SG\" \"TW\" \"US\" \"VN\"} and ip.geoip.continent eq \"AS\" and cf.threat_score le 5 and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"})",
+    "id": "25aa65bdcf774b549889cc98cf68a3cb",
+    "expression": "(ip.geoip.country eq \"VN\" and http.user_agent contains \"Mozilla/5.0 (Linux; Android\" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"} and cf.threat_score le 5 and ip.geoip.continent eq \"AS\") or (ip.geoip.country eq \"VN\" and http.user_agent contains \"Mozilla/5.0 (iPhone; CPU iPhone OS \" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"} and cf.threat_score le 5 and ip.geoip.continent eq \"AS\") or (ip.geoip.country eq \"VN\" and http.user_agent contains \"Mozilla/5.0 (Windows NT\" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"} and cf.threat_score le 5 and ip.geoip.continent eq \"AS\") or (ip.geoip.country eq \"VN\" and http.user_agent contains \"Mozilla/5.0 (Macintosh; Intel Mac OS\" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"} and cf.threat_score le 5 and ip.geoip.continent eq \"AS\")",
     "paused": false,
-    "description": "Managed Challenge is in Asia"
+    "description": "Captcha Việt Nam"
   }
   }'
